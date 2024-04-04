3fe2e13f799fbb72f842547b24fa8ac45ec9d1ff
Ajayambati1 <53788517+Ajayambati1@users.noreply.github.com>
2024-04-05 02:46:42 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -21,33 +21,6 @@ def excel_data(path,sheetslist):
             merge_dataframe.loc[merge_dataframe.shape[0]]=rows.to_dict()
     return merge_dataframe
 
-def check_password():
-    """Returns `True` if the user had the correct password."""
-
-    def password_entered():
-        """Checks whether a password entered by the user is correct."""
-        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
-            st.session_state["password_correct"] = True
-            del st.session_state["password"]  # Don't store the password.
-        else:
-            st.session_state["password_correct"] = False
-
-    # Return True if the password is validated.
-    if st.session_state.get("password_correct", False):
-        return True
-
-    # Show input for password.
-    st.text_input(
-        "Password", type="password", on_change=password_entered, key="password"
-    )
-    if "password_correct" in st.session_state:
-        st.error("😕 Password incorrect")
-    return False
-
-
-if not check_password():
-    st.stop()
-
 
 uploaded_file=st.sidebar.file_uploader("Upload your Excel File",type=['csv','xlsx'])
 if uploaded_file is not None:
